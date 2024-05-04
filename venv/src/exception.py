bd8a21621d84497303e462f9bc8006da2790e384
Ansita <ansitapanigrahi@gmail.com>
2024-05-04 11:00:08 +0530

    EDA and Problem Statements

--- venv/src/exception.py
+++ venv/src/exception.py
@@ -1,7 +1,6 @@
-import logging
-import sys
+import traceback
 
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error, error_detail: traceback):
     _, _, exc_tb = error_detail
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in Python script: {0}, line number: {1}, error message: {2}".format(
@@ -10,19 +9,13 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error, error_detail: sys):
+    def __init__(self, error, error_detail: traceback):
         self.error_message = error_message_detail(error, error_detail)
         super().__init__(self.error_message)
 
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    # Configure logging to file
-    logging.basicConfig(
-        filename='example.log',  # Specify the log file path
-        format="[%(asctime)s] %(levelname)s: %(message)s",
-        level=logging.INFO
-    )
+
     
 
